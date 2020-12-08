069d8577531a7d6403e89f941673c2b515f04080
Chandrakanth Thadkapally <c.thadkapally@nfm.com>
2020-12-07 18:20:10 -0600

    Cleanup Process

---
+++ Grpc.Clinet/BenchMarks.cs
@@ -0,0 +1,12 @@
+﻿namespace Grpc.Clinet
+{
+    public class BenchMarks
+    {
+        public double DataSize { get; set; }
+        public double NetworkTime { get; set; }
+        public double DeserializationTime { get; set; }
+        public double RenderingTime { get; set; }
+        public string Client { get; set; }
+    }
+
+}
--- Grpc.Clinet/Program.cs
+++ Grpc.Clinet/Program.cs
@@ -1,9 +1,11 @@
+using Grpc.Net.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Http;
+using Weather;
 
 namespace Grpc.Clinet
 {
@@ -18,7 +20,34 @@ namespace Grpc.Clinet
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.AddHostedService<Worker>(x =>
+                    {
+                        var stopwatch = new Stopwatch();
+                        var httpHandler = new BenchmarkHttpHandler(stopwatch, new HttpClientHandler());
+                        var httpClient = new HttpClient(httpHandler)
+                        {
+                            BaseAddress = new Uri("https://localhost:5004")
+                        };
+                        WeatherForecasts.WeatherForecastsClient CreateClient(string address, HttpMessageHandler handler)
+                        {
+                            var channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
+                            {
+                                HttpHandler = handler,
+                                MaxReceiveMessageSize = null
+                            });
+
+                            return new WeatherForecasts.WeatherForecastsClient(channel);
+                        }
+                        Func<string, Stopwatch, BenchMarks> benchMarks = (string clinetName, Stopwatch _stopwatch) => new BenchMarks
+                        {
+                            Client = clinetName,
+                            DataSize = Math.Round((double)(httpHandler.BytesRead!.Value) / 1024, 2),
+                            NetworkTime = Math.Round(httpHandler.HeadersReceivedElapsed!.Value.TotalSeconds, 2),
+                            DeserializationTime = Math.Round(_stopwatch.Elapsed.TotalSeconds - httpHandler.HeadersReceivedElapsed!.Value.TotalSeconds, 2)
+                        };
+                        return new Worker(x.GetService<ILogger<Worker>>(), httpClient, CreateClient("https://localhost:5001", httpHandler), CreateClient("https://localhost:5002", httpHandler), stopwatch, benchMarks);
+                    });
                 });
+
     }
 }
